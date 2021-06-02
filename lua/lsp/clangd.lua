200584b724c1fd66b1403b50b1e5a7f84765e46f
Shiny Mimikyu <mrtoniliu@gmail.com>
2021-06-02 07:58:57 +0100

    Update

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -1,5 +1,6 @@
 require'lspconfig'.clangd.setup {
     cmd = {DATA_PATH .. "/lspinstall/cpp/clangd/bin/clangd"},
+    filetypes = { "c", "cpp", "objc", "objcpp" },
     on_attach = require'lsp'.common_on_attach,
     handlers = {
         ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
